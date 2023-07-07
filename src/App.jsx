1b934b6405c9d7b91af1433bf99dce47450ca804
Adrian Fernández <vieri.vieri@hotmail.com>
2023-07-07 18:56:44 +0200

    new file:   package-lock.json modified:   src/App.css modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,119 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cardsOriginal = [
+    {
+      img: '',
+      name: 'Seniro forntend developer',
+      isNew: true,
+      isFeatured: true,
+      company: 'Photosnap',
+      date: new Date(),
+      isFullTime: true,
+      location: 'USA Only',
+      keywords: ['frontend', 'senior', 'html', 'css', 'javascript'],
+    },
+    {
+      img: '',
+      name: 'Test1 developer',
+      isNew: true,
+      isFeatured: true,
+      company: 'Photosnap',
+      date: new Date(),
+      isFullTime: true,
+      location: 'USA Only',
+      keywords: ['frontend', 'fullstack', 'html', 'react', 'sass'],
+    },
+    {
+      img: '',
+      name: 'Senior developer',
+      isNew: true,
+      isFeatured: true,
+      company: 'Testeo',
+      date: new Date(),
+      isFullTime: true,
+      location: 'USA Only',
+      keywords: ['midweight', 'junios', 'html', 'css', 'javascript'],
+    },
+    {
+      img: '',
+      name: 'Seniro forntend developer',
+      isNew: true,
+      isFeatured: true,
+      company: 'Manage',
+      date: new Date(),
+      isFullTime: true,
+      location: 'USA Only',
+      keywords: ['frontend', 'senior', 'html', 'backend', 'javascript'],
+    }
+  ];
+  const [searchList, setSearchList] = useState(['frontend']);
+  
+  const listCards = cardsOriginal;//cardsOriginal.filter( (card) => ( card.keywords.every( searchList ) ));
+  
+  function handleAddKeyword(keyword){
+    //setSearchList(searchList.push({keyword}));
+  }
+
+
+  //Componente Card
+  function Card ({img, name, isNew, isFeatured, company, date, isFullTime, location, keywords} ) {
+    
+
+    const listKeywords = keywords.map( keyword => (
+      <div className="jobTag" onClick={handleAddKeyword(keyword)}>{keyword}</div>
+    ));
+
+
+
+    return (
+      <div className="job-container">
+        <div className="jobs-info-section">
+            <img className="job-logo" src={img} alt={name} />
+
+            <div className="jobInfo-container">
+              <div className="jobInfo-header">
+                  <p className="jobInfo-company">{company}</p>
+              </div>
+              <p className="jobInfo-title">{name}</p>
+              <div className="jobInfo-footer">
+                  <p>{new Date().getTime() - date.getTime() }</p>
+                  <div className="oval"></div>
+                  <p>{isFullTime ? 'Full Time': 'Half time'}</p>
+                  <div className="oval"></div>
+                  <p>{location}</p>
+              </div>
+            </div>
+        </div>
+        <div className="jobTags-container">
+          {listKeywords} 
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+
+      <div className="header-banner">
+        <div className="filter-list-container">
+          <div className="filter-tags-container">
+            <div className="filterTag-container">
+              <div className="filterTag">{searchList.map(keyword => (<p>{keyword}</p>) )}</div>
+              <button className="filterTag-btn">X</button>
+            </div>
+          </div>
+          <p className="filterTag-clear">Clear</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="app-container">
+        <div className="jobs-container">
+          {listCards.map( ( card, index ) => (<Card key={index} img={card.img} name={card.name} isNew={card.isNew} isFeatured={card.isFeatured} company={card.company} date={card.date} isFullTime={card.isFullTime} location={card.location} keywords={card.keywords} /> ) ) }
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
