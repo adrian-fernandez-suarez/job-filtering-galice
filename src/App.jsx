0bc84771a2b4fb9dcfef164622c20dfc363ffad1
Adrian Fernández <vieri.vieri@hotmail.com>
2023-07-10 08:41:22 +0200

    Changes to be committed:     modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import cardsOriginal from "./assets/data.json"
 import './App.css'
 
 function App() {
-  const cardsOriginal = [
-    {
-      img: '',
-      name: 'Seniro forntend developer',
-      isNew: true,
-      isFeatured: true,
-      company: 'Photosnap',
-      date: new Date(),
-      isFullTime: true,
-      location: 'USA Only',
-      keywords: ['frontend', 'senior', 'html', 'css', 'javascript'],
-    },
-    {
-      img: '',
-      name: 'Test1 developer',
-      isNew: true,
-      isFeatured: true,
-      company: 'Photosnap',
-      date: new Date(),
-      isFullTime: true,
-      location: 'USA Only',
-      keywords: ['frontend', 'fullstack', 'html', 'react', 'sass'],
-    },
-    {
-      img: '',
-      name: 'Senior developer',
-      isNew: true,
-      isFeatured: true,
-      company: 'Testeo',
-      date: new Date(),
-      isFullTime: true,
-      location: 'USA Only',
-      keywords: ['midweight', 'junios', 'html', 'css', 'javascript'],
-    },
-    {
-      img: '',
-      name: 'Seniro forntend developer',
-      isNew: true,
-      isFeatured: true,
-      company: 'Manage',
-      date: new Date(),
-      isFullTime: true,
-      location: 'USA Only',
-      keywords: ['frontend', 'senior', 'html', 'backend', 'javascript'],
-    }
-  ];
-  const [searchList, setSearchList] = useState(['frontend']);
+  const [ searchList, setSearchList ] = useState( [ 'frontend' ] );
   
-  const listCards = cardsOriginal;//cardsOriginal.filter( (card) => ( card.keywords.every( searchList ) ));
+  const listCards = [...cardsOriginal]; //cardsOriginal.filter( (card) => ( card.keywords.every( searchList ) ));
   
   function handleAddKeyword(keyword){
     //setSearchList(searchList.push({keyword}));
@@ -60,14 +13,11 @@ function App() {
 
 
   //Componente Card
-  function Card ({img, name, isNew, isFeatured, company, date, isFullTime, location, keywords} ) {
+  function Card ({img, name, isNew, isFeatured, company, postedAt, isFullTime, location, keywords} ) {
     
-
     const listKeywords = keywords.map( keyword => (
       <div className="jobTag" onClick={handleAddKeyword(keyword)}>{keyword}</div>
     ));
-
-
 
     return (
       <div className="job-container">
@@ -80,7 +30,7 @@ function App() {
               </div>
               <p className="jobInfo-title">{name}</p>
               <div className="jobInfo-footer">
-                  <p>{new Date().getTime() - date.getTime() }</p>
+                  <p>{postedAt}</p>
                   <div className="oval"></div>
                   <p>{isFullTime ? 'Full Time': 'Half time'}</p>
                   <div className="oval"></div>
@@ -112,7 +62,7 @@ function App() {
 
       <div className="app-container">
         <div className="jobs-container">
-          {listCards.map( ( card, index ) => (<Card key={index} img={card.img} name={card.name} isNew={card.isNew} isFeatured={card.isFeatured} company={card.company} date={card.date} isFullTime={card.isFullTime} location={card.location} keywords={card.keywords} /> ) ) }
+          {listCards.map( card => (<Card key={card.id} img={card.logo} name={card.position} isNew={card.new} isFeatured={card.featured} company={card.company} date={card.postedAt} isFullTime={card.contract} location={card.location} keywords={card.languages} /> ) ) }
         </div>
       </div>
     </div>
